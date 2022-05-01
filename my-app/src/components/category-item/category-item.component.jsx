064ed781b4983b0346c378d5d8c2db61d22c3fe8
EvaTu <evatuw@gmail.com>
2022-05-01 13:31:38 -0400

    creating app directory and its child components

---
+++ my-app/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({category}) => {
+
+    return(
+       <div className="category-container">
+       <div className="background-image" style={{ backgroundImage: `url(${category.url})`}}/>
+       <div className="category-body-container">
+         <h2>{category.title}</h2>
+         <p>SHOP NOW</p>
+       </div>
+     </div>
+    )
+}
+
+export default CategoryItem
