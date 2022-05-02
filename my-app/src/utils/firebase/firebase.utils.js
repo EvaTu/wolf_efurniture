f74109aa7d3edb048eed56e7a76d2ec6fa66834b
EvaTu <evatuw@gmail.com>
2022-05-02 15:15:00 -0400

    using firebase auth and create users collection in firestore

--- my-app/src/utils/firebase/firebase.utils.js
+++ my-app/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, setDoc, getDoc} from 'firebase/firestore'
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBk8nsalEY25tBsU88lKqvSwi9G3Pu-fts",
@@ -20,3 +23,33 @@ provider.setCustomParameters({
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore(firebaseApp)
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+const userDocRef = doc(db, "users", userAuth.uid)
+// console.log(userDocRef)
+const userSnapshot = await getDoc(userDocRef)
+// console.log(userSnapshot)
+// console.log(userSnapshot.exists())
+
+
+if(!userSnapshot.exists()){
+  const {displayName, email} = userAuth
+  const createAt = new Date()
+  try{
+    await setDoc(userDocRef, {
+      displayName,
+      email,
+      createAt
+    })
+  }
+  catch(error){
+    console.log("error creating the user", error.message)
+  }
+
+}
+
+return userDocRef
+
+}
