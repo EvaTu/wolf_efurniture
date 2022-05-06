8d34f5bffc14b46d0bed7c651823524e2ae2602f
EvaTu <evatuw@gmail.com>
2022-05-05 23:06:05 -0400

    downgraded react from 18 to 17 due to strip issue

--- my-app/src/components/payment-form/payment-form.component.jsx
+++ my-app/src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,27 @@
 import "./payment-form.styles.scss";
-import { CardElement } from '@stripe/react-stripe-js';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+// import {PaymentElement} from '@stripe/react-stripe-js';
 import Button from "../button/button.component"
 
 const PaymentForm = () => {
+    const stripe = useStripe()
+    const elements = useElements()
+
+    const handlePayment = async (e) => {
+        e.preventDefault();
+
+        if(!stripe || !elements){
+            return
+        }
+
+        
+    }
 
     return(
-        <div>
-            <CardElement />
+        <div className="payment-form-container">
+            <h2 style={{ margin: "50px"}}>Credit Card Payment</h2>
+            {/* <PaymentElement /> */}
+            <CardElement className="form-container"/>
             <Button buttonType="inverted">Pay Now</Button>
         </div>
     )
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,8 +13,8 @@ import {stripePromise} from "./utils/stripe/stripe.utils"
 import {Elements} from '@stripe/react-stripe-js'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
@@ -27,7 +28,31 @@ root.render(
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
+  ,
+  document.getElementById('root')
 );
+
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <UserProvider>
+//         <CategoriesProvider>
+//           <CartProvider>
+//             <Elements stripe={stripePromise}>
+//               <App />
+//             </Elements>
+//           </CartProvider>
+//         </CategoriesProvider>
+//       </UserProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
